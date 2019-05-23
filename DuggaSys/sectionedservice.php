b9bc82942fa018ee0ad970319934c6be08ac2855
Pontus <pontus.g91@gmail.com>
2019-05-23 09:05:38 +0200

    testing using qid

--- DuggaSys/sectionedservice.php
+++ DuggaSys/sectionedservice.php
@@ -299,7 +299,7 @@ if($gradesys=="UNK") $gradesys=0;
 						}
 					}
 				}else if(strcmp($opt,"UPDATEDEADLINE")===0){
-					$deadlinequery = $pdo->prepare("UPDATE quiz SET deadline=:deadline WHERE id=:lid;");
+					$deadlinequery = $pdo->prepare("UPDATE quiz SET deadline=:deadline WHERE id=:qid;");
 					$deadlinequery->bindParam(':deadline',$deadline);
 					$deadlinequery->bindParam(':lid',$lid);
 
