32a54b78ff7dfb3ba8273f155115ab189364972e
Pontus <pontus.g91@gmail.com>
2019-05-23 10:03:20 +0200

    debugging

--- DuggaSys/sectionedservice.php
+++ DuggaSys/sectionedservice.php
@@ -302,7 +302,6 @@ if($gradesys=="UNK") $gradesys=0;
 					$deadlinequery = $pdo->prepare("UPDATE quiz SET deadline=:deadline WHERE cid=:cid;");
 					$deadlinequery->bindParam(':deadline',$deadline);
 					$deadlinequery->bindParam(':cid',$courseid);
-					$deadlinequery->bindParam(':vers', $coursevers);
 
 					if(!$deadlinequery->execute()){
 						$error=$deadlinequery->errorInfo();
