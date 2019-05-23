785c012b79be75ca4074c64924e51e652c384870
a17amama <a17amama@student.his.se>
2019-05-23 10:17:59 +0200

    Update resulted.js

--- DuggaSys/resulted.js
+++ DuggaSys/resulted.js
@@ -832,17 +832,7 @@ function renderCell(col, celldata, cellid) {
 			}
 			return str;	
 		} 
-	}
 	}	
-	if(filterList["showTeacher"]){
-		// First column (Fname/Lname/SSN)
-	if (col == "setTeacher") {
-		str = "<div class='resultTableCell resultTableNormal'>";
-		str += "<div class='resultTableText'>";
-		str += "<div style='font-weight:bold'>" + celldata.setTeacher + " " + "</div>";
-		str += "</div>";
-		return str;
-	}
 	
 	else if(filterList["passedDeadline"]){
 				// First column (Fname/Lname/SSN)
@@ -853,19 +843,7 @@ function renderCell(col, celldata, cellid) {
 				str += "<div>" + celldata.username + " / " + celldata.class + "</div>";
 				str += "</div>";
 				return str;
-
-	
-			}
-		 if(filterList["showTeacher"]){
-				// First column (Fname/Lname/SSN)
-			if (col == "setTeacher") {
-				str = "<div class='resultTableCell resultTableNormal'>";
-				str += "<div class='resultTableText'>";
-				str += "<div style='font-weight:bold'>" + celldata.setTeacher + " " + "</div>";
-				str += "</div>";
-				return str;
-			}
-			else if (filterGrade === "none" || celldata.grade === filterGrade) {
+			}else if (filterGrade === "none" || celldata.grade === filterGrade) {
 				// color based on pass,fail,pending,assigned,unassigned
 				str = "<div style='padding:10px;' class='resultTableCell ";
 				if (celldata.kind != 4 && celldata.needMarking == true && celldata.submitted > celldata.deadline) {
