72f67249e51072b922b8af95ac2d1c71f8ff2a9e
b18phibe <62876531+b18phibe@users.noreply.github.com>
2020-04-03 15:32:59 +0200

    all good
    Now shit will hopfoly work.

--- DuggaSys/download.php
+++ DuggaSys/download.php
@@ -5,50 +5,44 @@
      * every file sould be put in one zip file and then downloaded*
      **************************************************************/
 		// Used tutorial https://learncodeweb.com/php/select-and-download-multi-files-in-zip-format-with-php/
-		include_once "../Shared/basic.php";
-		include_once "../Shared/sessions.php";
+		include_once	"../Shared/basic.php";
+		include_once	"../Shared/sessions.php";
 		session_start();
 
 		pdoConnect(); // Connect to database and start session
 		
 	
 		// Get real path for our folder
-		$cid				= $_SESSION['courseid'];
-		$vers 			= $_SESSION['coursevers'];
-		echo "cid: " . $cid . ";vers: " . $vers;
-		$pathToVersionIndependence = '/courses/' . $cid . '/versionIndependence/';
-		$pathToActiveVersionOfCourse = '/courses/' . $cid . '/' . $vers. '/';
-		echo "pat1: "	.	$pathToVersionIndependence	.	"path2: "	. $pathToActiveVersionOfCourse;
-		$filename   =   "courseID-". $cid ."_version-". $vers. "_All_files.zip";
+		$cid	= $_SESSION['courseid'];
+		$vers	= $_SESSION['coursevers'];
+		$pathToVersionIndependence	=	'/courses/'	.	$cid	.	'/versionIndependence/';
+		$pathToActiveVersionOfCourse	=	'/courses/' . $cid .	'/'	.	$vers	.	'/';
+		$zipcreated	=	"./downloads/courseID-"	.	$cid	.	"_version-"	.	$vers	.	"_All_files.zip";
 
-		
-  
-		// Enter the name to creating zipped directory 
-		$zipcreated = $filename; 
 		// Create new zip class 
-		$zip = new ZipArchive; 
+		$zip	=	new	ZipArchive; 
    
-		if($zip -> open($zipcreated, ZipArchive::CREATE ) === TRUE) { 
+		if($zip	->	open($zipcreated,	ZipArchive::CREATE	)	===	TRUE){ 
 			chdir('../');
-			$currcvd = getcwd();
+			$currcvd	=	getcwd();
 
 			
 			// Enter the name of directory 
-			$pathdir = $currcvd.$pathToVersionIndependence;  
+			$pathdir	=	$currcvd.$pathToVersionIndependence;  
 
 
 			// Store the path into the variable 
-			if ($dir = opendir($pathdir)){ 
-				while(false !== $file = readdir($dir)) { 
-					if($file != "." && $file	!=	".."	&&	!is_dir($pathdir."/".$file)) { 
-							$zip -> addFile($pathdir.$file, $file); 
+			if($dir	=	opendir($pathdir)){ 
+				while(false	!==	$file	=	readdir($dir)){ 
+					if($file	!=	"."	&&	$file	!=	".."	&&	!is_dir($pathdir."/".$file)){ 
+							$zip	->	addFile($pathdir.$file,	$file); 
 					} 
 				}
 				closedir($dir);
 			}
 
 			// Enter the name of directory 2
-			$pathdir = $currcvd.$pathToActiveVersionOfCourse;  
+			$pathdir	=	$currcvd.$pathToActiveVersionOfCourse;  
 			
 			// Store the path into the variable 
 			if($dir = opendir($pathdir)){ 
@@ -60,17 +54,25 @@
 				closedir($dir);
 			}	
 
-			$zip ->close(); 
-			
-			//header($_SERVER['SERVER_PROTOCOL'].' 200 OK');
-			header('Content-Type: application/zip');
-			//header("Content-Transfer-Encoding: Binary");
-			header('Content-Length: ' . filesize($filename));
-			header("Content-Disposition: attachment; filename=$filename");
-			
-			readfile($zip);
-			exit;
-
+			$zip	->	close(); 
+			chdir('./DuggaSYS');
+			if(file_exists($zipcreated)) {
+				echo "in file download";
+				header('Content-Description: File Transfer');
+				header('Content-Type: application/zip');
+				header('Content-Disposition: attachment; filename='.basename($zipcreated));
+				header('Content-Transfer-Encoding: binary');
+				header('Expires: 0');
+				header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+				header('Pragma: public');
+				header('Content-Length: ' . filesize($zipcreated));
+				ob_clean();
+				flush();
+				readfile($zipcreated);
+				exit;
+			}else {
+				echo "failed to find a file";
+			}
 		}
 		 
 		
@@ -80,7 +82,7 @@
 </head>
 <body>
 <?php
-    echo "<script>window.location.replace('sectioned.php?cid=" . $cid . "&coursevers=" . $vers . "');</script>"; //update page, redirect to "sectioned.php" with the variables sent for course id and version id
+		echo	"<script>window.location.replace('sectioned.php?cid="	.	$cid	.	"&coursevers="	.	$vers	.	"');</script>";	//update page, redirect to "sectioned.php" with the variables sent for course id and version id
 ?>
 </body>
 </html>
