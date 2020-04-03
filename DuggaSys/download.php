bd232298d51de63bc945c16c64c7e3e83fb1967d
b18phibe <62876531+b18phibe@users.noreply.github.com>
2020-04-03 09:59:10 +0200

    a html 500 error in the download.php
    it looks like the download file are as it shoud but i get a html 500 error i do not understand. #7765

--- DuggaSys/download.php
+++ DuggaSys/download.php
@@ -1,38 +1,66 @@
 <?php
-echo 'hello;'
     /***********************DOCUMENTATION**************************
      * This file should make downloading of all material          *
      * of a singel course doable given we know the courseid       *
      * every file sould be put in one zip file and then downloaded*
      **************************************************************/
 		// Used tutorial https://learncodeweb.com/php/select-and-download-multi-files-in-zip-format-with-php/
-		
-		
-			
-				$db         = new PDO('mysql:dbname=lenasys;host=localhost', 'root', 'Hundar');
-				$courseName	=	$db->query('SELECT coursename FROM course WHERE cid'.$_SESSION['courseid'].'');
-				$filename   =   'course' + $courseName + 'All files.zip';
-				
-				$fileQry    =   $db->query('SELECT * FROM files WHERE cid'.$_SESSION['courseid'].'');
-				 
-				$zip = new ZipArchive;
+		include_once "../Shared/basic.php";
+		include_once "../Shared/sessions.php";
+		session_start();
 
-				consol.log ( $courseName +$filename+$fileQry);
-				if ($zip->open($filename,  ZipArchive::CREATE)){
-						while($row  =   $fileQry->fetch_assoc()){
-								$zip->addFile(getcwd().'/'.'uploads/'.$row['filename'], $row['filename']);
-						}
-						$zip->close();
-						header("Content-type: application/zip"); 
-						header("Content-Disposition: attachment; filename=$filename");
-						header("Content-length: " . filesize($filename));
-						header("Pragma: no-cache"); 
-						header("Expires: 0"); 
-						readfile("$filename");
-						unlink($filename);
-				}else{
-					 echo 'Failed!';
-				}
+		pdoConnect(); // Connect to database and start session
 		
+		// Get real path for our folder
+		$cid				= $_SESSION['courseid'];
+		$vers 			= $_SESSION('coursevers');
+		$courseName	=	'hey';
+		$pathToVersionIndependence = "courses/" . $cid . "/versionIndependence/";
+		$pathToActiveVersionOfCourse = "courses/" . $cid . "/" . $vers . "/";
+		$filename   =   'course/' . $courseName . '/All/files.zip';
+
+		// Enter the name of directory 
+		$pathdir = $pathToVersionIndependence;  
+  
+		// Enter the name to creating zipped directory 
+		$zipcreated = $filename; 
+  
+		// Create new zip class 
+		$zip = new ZipArchive; 
+   
+		if($zip -> open($zipcreated, ZipArchive::CREATE ) === TRUE) { 
+      
+    // Store the path into the variable 
+    $dir = opendir($pathdir); 
+       
+    while($file = readdir($dir)) { 
+        if(is_file($pathdir.$file)) { 
+            $zip -> addFile($pathdir.$file, $file); 
+        } 
+		} 
+		
+		// Enter the name of directory 2
+		$pathdir = $pathToActiveVersionOfCourse;  
+
+		// Store the path into the variable 
+		$dir = opendir($pathdir); 
+
+		while($file = readdir($dir)) { 
+			if(is_file($pathdir.$file)) { 
+					$zip -> addFile($pathdir.$file, $file); 
+			} 
+	} 
+
+    $zip ->close(); 
+} 
 		
 		?>
+<html>
+<head>
+</head>
+<body>
+<?php
+    echo "<script>window.location.replace('sectioned.php?cid=" . $cid . "&coursevers=" . $vers . "');</script>"; //update page, redirect to "sectioned.php" with the variables sent for course id and version id
+?>
+</body>
+</html>
